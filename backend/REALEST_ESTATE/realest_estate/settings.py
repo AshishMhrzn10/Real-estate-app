57295233a5d555332a9b1f72526670101d363d2a
Ashish Maharjan <amaharjan1032@gmail.com>
2020-08-31 19:50:57 +0545

    Realtors app setup

--- backend/REALEST_ESTATE/realest_estate/settings.py
+++ backend/REALEST_ESTATE/realest_estate/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'accounts',
+    'realtors',
 ]
 
 MIDDLEWARE = [
--- backend/REALEST_ESTATE/realest_estate/urls.py
+++ backend/REALEST_ESTATE/realest_estate/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
     path('api/accounts/', include('accounts.urls')),
+    path('api/realtors/', include('realtors.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
