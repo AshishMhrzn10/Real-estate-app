b92b3c21c8ae197ac64cf90f2cf45ac1e201b651
Ashish Maharjan <amaharjan1032@gmail.com>
2020-09-01 21:52:04 +0545

    Actions, reducers,store in react done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,22 +10,27 @@ import SignUp from "./containers/SignUp";
 import NotFound from "./components/NotFound";
 import Layout from "./hocs/Layout";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./sass/main.scss";
 
 const App = () => (
-  <Router>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about/" component={About} />
-        <Route exact path="/contact/" component={Contact} />
-        <Route exact path="/listings/" component={Listings} />
-        <Route exact path="/listings/:id/" component={ListingDetail} />
-        <Route exact path="/login/" component={Login} />
-        <Route exact path="/signup/" component={SignUp} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about/" component={About} />
+          <Route exact path="/contact/" component={Contact} />
+          <Route exact path="/listings/" component={Listings} />
+          <Route exact path="/listings/:id/" component={ListingDetail} />
+          <Route exact path="/login/" component={Login} />
+          <Route exact path="/signup/" component={SignUp} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </Router>
+  </Provider>
 );
 export default App;
