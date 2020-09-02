b73f317b86249162423b63808d7d5162a5853806
Ashish Maharjan <amaharjan1032@gmail.com>
2020-09-02 15:10:38 +0545

    navbar, alert and Scss files done

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,88 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../actions/auth";
+import Alert from "./Alert";
+import PropTypes from "prop-types";
 
-const navbar = () => <div>navbar</div>;
-export default navbar;
+const navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authlinks = (
+    <a className="navbar__top__auth__link" onClick={logout} href="#!">
+      Logout
+    </a>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <Link className="navbar__top__auth__link" to="/login/">
+        Login
+      </Link>
+      <Link className="navbar__top__auth__link" to="/signup/">
+        Sign up
+      </Link>
+    </Fragment>
+  );
+
+  return (
+    <Fragment>
+      <nav className="navbar">
+        <div className="navbar__top">
+          <div className="navbar__top__logo">
+            <Link className="navbar__top__logo__link" to="/">
+              Realest Estate
+            </Link>
+          </div>
+          <div className="navbar__top__auth">
+            {!loading && (
+              <Fragment>{isAuthenticated ? authlinks : guestLinks}</Fragment>
+            )}
+          </div>
+        </div>
+        <div className="navbar__bottom">
+          <li className="navbar__bottom__item">
+            <NavLink className="navbar__bottom__item__link" exact to="/">
+              Home
+            </NavLink>
+          </li>
+
+          <li className="navbar__bottom__item">
+            <NavLink
+              className="navbar__bottom__item__link"
+              exact
+              to="/listings/"
+            >
+              Listings
+            </NavLink>
+          </li>
+
+          <li className="navbar__bottom__item">
+            <NavLink className="navbar__bottom__item__link" exact to="/about/">
+              About
+            </NavLink>
+          </li>
+
+          <li className="navbar__bottom__item">
+            <NavLink
+              className="navbar__bottom__item__link"
+              exact
+              to="/contact/"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </div>
+      </nav>
+      <Alert />
+    </Fragment>
+  );
+};
+
+navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(navbar);
