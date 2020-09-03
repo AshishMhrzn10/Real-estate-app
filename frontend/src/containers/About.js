5479ea17d78b39434fdad8dfef413f85938dc224
Ashish Maharjan <amaharjan1032@gmail.com>
2020-09-03 22:19:08 +0545

    About and contact done

--- frontend/src/containers/About.js
+++ frontend/src/containers/About.js
@@ -1,4 +1,154 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import { Helmet } from "react-helmet";
+import axios from "axios";
+import House from "../assets/images/house.jpg";
 
-const about = () => <div>about</div>;
-export default about;
+const About = () => {
+  const [topSeller, setTopSeller] = useState([]);
+  const [realtors, setRealtors] = useState([]);
+
+  useEffect(() => {
+    axios.defaults.headers = {
+      "Content-Type": "application/json",
+    };
+    const getTopSeller = async () => {
+      try {
+        const res = await axios.get(
+          "http://127.0.0.1:8000/api/realtors/topseller/"
+        );
+        setTopSeller(res.data);
+      } catch (err) {}
+    };
+
+    getTopSeller();
+  }, []);
+
+  useEffect(() => {
+    axios.defaults.headers = {
+      "Content-Type": "application/json",
+    };
+
+    const getRealtors = async () => {
+      try {
+        const res = await axios.get("http://127.0.0.1:8000/api/realtors/");
+        setRealtors(res.data);
+      } catch (err) {}
+    };
+    getRealtors();
+  }, []);
+
+  const getAllRealtors = () => {
+    let allRealtors = [];
+    let results = [];
+
+    realtors.map((realtor) => {
+      return allRealtors.push(
+        <Fragment key={realtor.id}>
+          <div className="about__display">
+            <img className="about__display__image" src={realtor.photo} alt="" />
+          </div>
+          <h3 className="about__realtor">{realtor.name}</h3>
+          <p className="about__contact">{realtor.phone}</p>
+          <p className="about__contact">{realtor.email}</p>
+          <p className="about__about">{realtor.description}</p>
+        </Fragment>
+      );
+    });
+
+    for (let i = 0; i < realtors.length; i += 3) {
+      results.push(
+        <div key={i} className="row">
+          <div className="col-1-of-3">{allRealtors[i]}</div>
+          <div className="col-1-of-3">
+            {allRealtors[i + 1] ? allRealtors[i + 1] : null}
+          </div>
+          <div className="col-1-of-3">
+            {allRealtors[i + 2] ? allRealtors[i + 2] : null}
+          </div>
+        </div>
+      );
+    }
+
+    return results;
+  };
+
+  const getTopSeller = () => {
+    let result = [];
+    topSeller.map((seller) => {
+      return result.push(
+        <Fragment key={seller.id}>
+          <div className="about__display">
+            <img className="about__display__image" src={seller.photo} alt="" />
+          </div>
+          <h3 className="about__topseller">Top Seller:</h3>
+          <p className="about__realtor">{seller.name}</p>
+          <p className="about__contact">{seller.phone}</p>
+          <p className="about__contact">{seller.email}</p>
+          <p className="about__about">{seller.description}</p>
+        </Fragment>
+      );
+    });
+
+    return result;
+  };
+  return (
+    <main className="about">
+      <Helmet>
+        <title> Realest Estate - About</title>
+        <meta name="description" content="About us" />
+      </Helmet>
+      <header className="about__header">
+        <h1 className="about__heading">About Realest Estate</h1>
+      </header>
+      <section className="about__info">
+        <div className="row">
+          <div className="col-3-of-4">
+            <h2 className="about__subheading">
+              We find the perfect home for you
+            </h2>
+            <p className="about__paragraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              vel ultricies orci, a sollicitudin magna. Proin et venenatis elit.
+              Mauris molestie nisi eu tortor porttitor rhoncus. Suspendisse
+              semper purus ac ante sagittis mollis. Mauris vestibulum bibendum
+              purus quis elementum. Aliquam interdum scelerisque quam at rutrum.
+              Vestibulum ut ligula ullamcorper, lacinia diam at, rhoncus risus.
+              Etiam at dolor sollicitudin, tincidunt est vitae, lacinia urna.
+              Etiam fringilla enim et fermentum dapibus. Maecenas et molestie
+              lacus. Proin vel erat justo. Nullam congue erat nec orci auctor,
+              id feugiat libero molestie. Etiam in dui quam.
+            </p>
+            <div className="about__display">
+              <img className="about__display__image" src={House} alt="" />
+            </div>
+            <p className="about__paragraph">
+              Phasellus efficitur varius pharetra. Donec auctor ultricies risus
+              rutrum lacinia. Curabitur eleifend dui quis mi consectetur, nec
+              egestas ipsum tempus. Quisque id mauris sollicitudin, auctor magna
+              quis, viverra quam. Aliquam lacinia turpis non massa semper
+              consectetur a nec massa. Pellentesque vehicula dolor sit amet nisl
+              semper tincidunt. Morbi luctus metus a hendrerit maximus.
+              Suspendisse tincidunt ornare elit, in accumsan erat commodo et.
+              Aliquam id metus vel justo pharetra convallis. Ut blandit neque
+              sed sagittis faucibus. Nullam nec odio a justo suscipit placerat.
+              Morbi ut dolor nulla. Ut sed suscipit nisi. Donec viverra tortor
+              eget vestibulum tincidunt. Curabitur maximus dolor nisi, at
+              pharetra eros pellentesque in. Nullam imperdiet orci risus, vel
+              tempus ante dapibus ultrices.
+            </p>
+          </div>
+
+          <div className="col-1-of-4">{getTopSeller()}</div>
+        </div>
+      </section>
+
+      <section className="about__team">
+        <div className="row">
+          <h2 className="about__subheading">Meet our Awesome Team</h2>
+        </div>
+        {getAllRealtors()}
+      </section>
+    </main>
+  );
+};
+export default About;
--- frontend/src/containers/Contact.js
+++ frontend/src/containers/Contact.js
@@ -1,4 +1,129 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
+import axios from "axios";
+import { connect } from "react-redux";
+import { setAlert } from "../actions/alert";
+import Loader from "react-loader-spinner";
+import PropTypes from "prop-types";
 
-const contact = () => <div>contact</div>;
-export default contact;
+const Contact = ({ setAlert }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const { name, email, subject, message } = formData;
+
+  const [loading, setLoading] = useState(false);
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios.defaults.headers = {
+      "Content-Type": "application/json",
+    };
+    setLoading(true);
+    axios
+      .post("http://127.0.0.1:8000/api/contacts/", {
+        name,
+        email,
+        subject,
+        message,
+      })
+      .then((res) => {
+        setAlert("Message sent", "success");
+        setLoading(false);
+        window.scrollTo(0, 0);
+      })
+      .catch((err) => {
+        setAlert("Error sending message", "error");
+        setLoading(false);
+        window.scrollTo(0, 0);
+      });
+  };
+
+  return (
+    <div className="contact">
+      <Helmet>
+        <title> Realest Estate - Contact</title>
+        <meta name="description" content="Contact us" />
+      </Helmet>
+      <form className="contact__form" onSubmit={(e) => onSubmit(e)}>
+        <label className="contact__form__label" htmlFor="name">
+          Name*
+        </label>
+        <input
+          className="contact__form__input"
+          type="text"
+          name="name"
+          placeholder="Full name"
+          onChange={(e) => onChange(e)}
+          value={name}
+          required
+        ></input>
+
+        <label className="contact__form__label" htmlFor="email">
+          Email*
+        </label>
+        <input
+          className="contact__form__input"
+          type="email"
+          name="email"
+          placeholder="example@email.com"
+          onChange={(e) => onChange(e)}
+          value={email}
+          required
+        ></input>
+
+        <label className="contact__form__label" htmlFor="subject">
+          Subject*
+        </label>
+        <input
+          className="contact__form__input"
+          type="text"
+          name="subject"
+          placeholder="Subject"
+          onChange={(e) => onChange(e)}
+          value={subject}
+          required
+        ></input>
+
+        <label className="contact__form__label" htmlFor="message">
+          Message
+        </label>
+        <textarea
+          className="contact__form__textarea"
+          name="message"
+          cols="30"
+          rows="10"
+          placeholder="Type your message"
+          onChange={(e) => onChange(e)}
+          value={message}
+        ></textarea>
+
+        {loading ? (
+          <div className="contact__form__loader">
+            <Loader type="Oval" color="#424242" height={50} width={50} />
+          </div>
+        ) : (
+          <button className="contact__form__button" htmltype="submit">
+            Send
+          </button>
+        )}
+      </form>
+    </div>
+  );
+};
+
+Contact.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+export default connect(null, { setAlert })(Contact);
